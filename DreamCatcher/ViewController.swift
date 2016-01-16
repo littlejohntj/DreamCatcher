446b1c4e5837c281698562761f6a008fc871c58e
TJ Littlejohn <littlejohn.tj@gmail.com>
2016-01-16 00:18:21 -0500

    Initial Commit

---
+++ DreamCatcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamCatcher
+//
+//  Created by Todd Littlejohn on 1/16/16.
+//  Copyright © 2016 Todd Littlejohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
